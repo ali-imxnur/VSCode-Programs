d27490ee00d5b1a122732cdb92ae234ba98a7e30
imaanx <imanur.8670@gmail.com>
2024-08-19 00:11:28 +0530

    FirstJava

---
+++ Helloworld.java
@@ -0,0 +1,13 @@
+class Helloworld
+{
+    void h1()
+    {
+        System.out.println("Hello World");
+    }
+    
+    public static void main(String [] args)
+    {
+       Helloworld h = new Helloworld();
+       h.h1();
+    }
+}
