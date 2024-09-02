0453d47b7a8caff93cbbb58bcbe6cb79c6e78f2a
Imanur Ali <113719461+ali-imxnur@users.noreply.github.com>
2024-09-02 08:09:40 +0000

    added variables

--- Basic JS/01_basic.js
+++ Basic JS/01_basic.js
@@ -1,1 +1,8 @@
-console.log("Imanur Ali");
+let account_Id = "3232"
+let account_Email = "imanur.ali@gmail.com"
+let account_Name = "Imanur Ali"
+
+console.table([account_Id, account_Email, account_Name])
+
+
+console.log("Push to Github");
